eb23a94217ec6f900ff8122e963effcfaa08e0ce
Gloria Washington <gloriaewashington@gmail.com>
2018-10-09 19:38:01 -0400

    Combined update and add html into one file

--- productmanager/inventory.py
+++ productmanager/inventory.py
@@ -21,7 +21,7 @@ def get_product(id):
     if product is None:
         flash('Product does not exist. Please try again.')
     #Turn product to dictionary
-    return product
+    return dict(product)
 
 
 def get_value():
@@ -54,10 +54,18 @@ def index():
     return render_template('inventory/index.html', products = products, inventory_val=inventory_val)
 
 
+
 @bp.route('/add', methods=('GET', 'POST'))
-def add():
+@bp.route('/<int:id>/update', methods=('GET','POST'))
+def add(id=[]):
     # Combine Add and Update. If there is a product ID, route it to update
     #else, route it to add
+    formTitle = 'Add Product'
+    product = ''
+    if id:
+      product = get_product(id)
+      formTitle = 'Edit Product'
+
     if request.method == 'POST':
         if request.form['button'] == 'Save':
             productName = request.form['productName']
@@ -76,7 +84,8 @@ def add():
 
             if error is not None:
                 flash(error)
-            else:
+
+            if not id:
                 db=get_db()
                 db.execute(
                     'INSERT INTO product (productName, quantity, price)'
@@ -85,42 +94,21 @@ def add():
                 db.commit()
                 return redirect(url_for('inventory.index'))
 
+            else:
+                db=get_db()
+                db.execute(
+                    'UPDATE product SET productName = ?, quantity = ?, price = ?'
+                    'WHERE id = ?',
+                    (productName, quantity, price, id)
+                )
+                db.commit()
+                return redirect(url_for('inventory.index'))
+
+
         elif request.form['button'] == 'Back':
             return redirect(url_for('inventory.index'))
 
-    return render_template('inventory/add.html')
-
-@bp.route('/<int:id>/update', methods=('GET','POST'))
-def update(id):
-    product = get_product(id)
-    if request.method == 'POST':
-        productName = request.form['productName']
-        quantity = request.form['quantity']
-        price = request.form['price']
-        error = None
-
-        if not productName:
-            error = 'Product name is required.'
-        
-        if not quantity:
-            error = 'Amount of products required.'
-        
-        if not price:
-            error = 'Price is required.'
-        
-        if error is not None:
-            flash(error)
-
-        else:
-            db=get_db()
-            db.execute(
-                'UPDATE product SET productName = ?, quantity = ?, price = ?'
-                'WHERE id = ?',
-                (productName, quantity, price, id)
-            )
-            db.commit()
-            return redirect(url_for('inventory.index'))
-    return render_template('inventory/update.html', product=dict(product))
+    return render_template('inventory/update.html', product=product, formTitle=formTitle)
 
 @bp.route('/<int:id>/delete', methods=('GET', 'POST',))
 def delete(id):
